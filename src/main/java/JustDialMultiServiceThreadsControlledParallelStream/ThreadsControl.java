f804e396818d692ddf64f18aa206853249c3f72e
matcdac <matcdac@gmail.com>
2016-08-23 19:02:11 +0530

    Adding code for JustDial MultiService ThreadsControlled ParallelStream. The ThreadsControlled technique is my implementation for ThreadPool. Adding some more data using the same.

---
+++ src/main/java/JustDialMultiServiceThreadsControlledParallelStream/ThreadsControl.java
@@ -0,0 +1,56 @@
+package JustDialMultiServiceThreadsControlledParallelStream;
+
+import java.lang.Thread.State;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadsControl {
+	
+	private List<Runnable> listOfRunnable;
+	private List<Thread> threads;
+	private byte maxThreadsToBeActive;
+	private byte currentActiveThreads;
+	
+	public ThreadsControl(byte maxThreadsToBeActive) {
+		listOfRunnable = new ArrayList<Runnable>();
+		threads = new ArrayList<Thread>();
+		currentActiveThreads = (byte)0;
+		this.maxThreadsToBeActive = maxThreadsToBeActive;
+	}
+	
+	public void add(Runnable code) {
+		listOfRunnable.add(code);
+	}
+	
+	private void startOneThreadFromList() {
+		Runnable code = listOfRunnable.get(0);
+		listOfRunnable.remove(0);
+		Thread thread = new Thread(code);
+		thread.start();
+		threads.add(thread);
+		currentActiveThreads++;
+	}
+	
+	private void checkForTerminatedThreadsAndRemoveThemFromList() {
+		for(Thread thread : threads) {
+			State state = thread.getState();
+			if(Thread.State.TERMINATED.compareTo(state)==0) {
+				threads.remove(thread);
+				currentActiveThreads--;
+				break;
+			}
+		}
+	}
+	
+	public void start() {
+		while(listOfRunnable.size() > 0) {
+			while(currentActiveThreads<maxThreadsToBeActive || (listOfRunnable.size()>0 && maxThreadsToBeActive>listOfRunnable.size()) ) {
+				startOneThreadFromList();
+			}
+			if(currentActiveThreads <= maxThreadsToBeActive) {
+				checkForTerminatedThreadsAndRemoveThemFromList();
+			}
+		}
+	}
+	
+}
